043eedba90c6f5466ae054216c03bc92dcb37dd4
bumbleblo <bumbleblo2013@gmail.com>
2017-06-22 02:19:58 -0300

    Heaps

---
+++ Heaps/heap_max_int.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX(A,B) (A>=B)? A : ;
+
+int get_altura(node *root){
+	int cont=0;
+	cont = MAX(get_altura,cont);
+	return 1;
+}
+
+node* insert_heap(
+
+int main(){
+
+ return 0;
+}
